1ca6cf588cab21e0640ebb57fd849267d168a9eb
Neil Anglim <neiljanglim@gmail.com>
2023-10-12 17:37:18 +0100

    completing w3 assignment, unformatted json file

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,1 +1,12 @@
+import requests
+import json
 
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def getAll():
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll()), file = fp)
