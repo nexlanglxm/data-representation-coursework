976aa9fb8e8020a4b76936ab8fe8ac54cd0d89cb
Neil Anglim <neiljanglim@gmail.com>
2023-10-13 14:41:23 +0100

    working through the lab

--- labs/w4.1requests.py
+++ labs/w4.1requests.py
@@ -3,7 +3,7 @@ import json
 
 url = "https://andrewbeatty1.pythonanywhere.com/books"
 
-def readBooks():
+def readbooks():
     response = requests.get(url)
     #got this idea from "https://stackoverflow.com/questions/54087303/python-requests-how-to-check-for-200-ok"
     if response.status_code == 200:
@@ -13,5 +13,24 @@ def readBooks():
     return response.json()
 
 if __name__ == "__main__":
-    print(readBooks())
+    print(readbooks())
+
+def readbook(id):
+    geturl = url + "/" + str(id)
+    response = requests.get(geturl)
+# we could do checking for correct response code here
+    if response.status_code == 200:
+        print("Status code '200', all is good.")
+    return response.json()
+
+def createbook(book):
+    response = requests.post(url, json=book)
+    if response.status_code == 200:
+        print("Status code '200', all is good.")
+    return response.json()
+
+def updatebook(id, book):
+    puturl = url + "/" + str(id)
+    response = requests.put(puturl, json=book)
+    return response.json()
 
