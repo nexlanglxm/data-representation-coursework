8e403b661d5701c0ea87f7a160d33d3e5cb071af
Neil Anglim <neiljanglim@gmail.com>
2023-10-25 15:00:41 +0100

    lab work, clone repo

---
+++ labs/topic5pygithub.py
@@ -0,0 +1,25 @@
+from github import Github
+from config import gitconfig
+import requests
+
+apikey = gitconfig['gitkey']
+g = Github(apikey)
+print(g)
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+repo = g.get_repo("nexlanglxm/thepublicone")
+# print(repo.clone_url)
+
+fileInfo = repo.get_contents("test.txt")
+urlofFile = fileInfo.download_url
+# print (urlofFile)
+
+response = requests.get(urlofFile)
+contentofFile = response.text
+# print (contentofFile)
+
+newContents = contentofFile + "\n more shtuff, \n and more"
+# print(newContents)
+
+gitHubresponse = repo.update_file(fileInfo.path,"updated via prog",newContents,fileInfo.sha)
+#print (gitHubresponse)
