9845fcc74b168b130eead4e96ddd0bc3ab5e0eb3
Neil Anglim <neiljanglim@gmail.com>
2023-11-24 09:00:40 +0000

    adding spotify route

--- project/app.py
+++ project/app.py
@@ -6,6 +6,7 @@ app.secret_key = 'your_secret_key'
 
 # Registering blueprints for different modules
 from routes.auth import auth_bp
+from routes.spotify import spotify_bp
 
 # rendering the html
 @app.route('/')
@@ -13,6 +14,7 @@ def index():
     return render_template('index.html')
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(spotify_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
