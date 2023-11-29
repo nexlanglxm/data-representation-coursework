5659626db59f600c255570a4cf925db0f7857217
Neil Anglim <neiljanglim@gmail.com>
2023-11-29 18:25:49 +0000

    moving all auth into appropriate route

--- project/routes/auth.py
+++ project/routes/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, session, request, jsonify
+import requests
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -19,10 +20,34 @@ def login():
     }
     return redirect(f"{auth_url}?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={scopes}")
 
+@auth_bp.route('/callback')
+def callback():
+    code = request.args.get('code')
+    access_token = get_access_token(code)  # Utilize the defined function from spotify.py
+    session['access_token'] = access_token
+    return 'Authentication successful. Access token obtained.'
 
 @auth_bp.route('/logout')
 def logout():
     # Placeholder logic for logout
     return 'Logout Page'
 
-# Other authentication-related routes
+# Define the get_access_token function here
+def get_access_token(code):
+    token_url = 'https://accounts.spotify.com/api/token'
+    token_data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }
+    token_response = requests.post(token_url, data=token_data)
+    access_token = token_response.json().get('access_token')
+    return access_token
+
+# Define the get_user_profile function here
+def get_user_profile(access_token):
+    headers = {'Authorization': f'Bearer {access_token}'}
+    profile_response = requests.get('https://api.spotify.com/v1/me', headers=headers)
+    return profile_response.json()
