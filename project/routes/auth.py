e929edfb7bb1cea6ab7a34e07b810b76155010dd
Neil Anglim <neiljanglim@gmail.com>
2023-12-14 15:40:41 +0000

    implementing code verifier, pkce

--- project/routes/auth.py
+++ project/routes/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, redirect, session, request, jsonify, url_for
 import requests
+import hashlib
+import base64
+import secrets
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -7,24 +10,41 @@ CLIENT_ID = '91ad79bc13fe4e21b070ff8c7a8271ca'
 CLIENT_SECRET = 'placeholder'
 REDIRECT_URI = 'http://127.0.0.1:5000/callback'
 
+def generate_code_verifier():
+    return base64.urlsafe_b64encode(secrets.token_bytes(32)).decode('utf-8').rstrip('=')
+
+def generate_code_challenge(code_verifier):
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    return base64.urlsafe_b64encode(code_challenge).decode('utf-8').rstrip('=')
+
 @auth_bp.route('/login')
 def login():
-    # Redirect users to Spotify's authorization URL
+    code_verifier = generate_code_verifier()
+    code_challenge = generate_code_challenge(code_verifier)
+    
+    session['code_verifier'] = code_verifier
+
     auth_url = 'https://accounts.spotify.com/authorize'
     scopes = 'user-read-private user-read-email'  # Define required scopes
     auth_params = {
         'client_id': CLIENT_ID,
         'response_type': 'code',
         'redirect_uri': REDIRECT_URI,
-        'scope': scopes
+        'scope': scopes,
+        'code_challenge': code_challenge,
+        'code_challenge_method': 'S256'
     }
-    return redirect(f"{auth_url}?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={scopes}")
+    
+    return redirect(f"{auth_url}?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={scopes}&code_challenge={code_challenge}&code_challenge_method=S256")
 
 @auth_bp.route('/callback')
 def callback():
     code = request.args.get('code')
-    access_token = get_access_token(code) 
+    state = request.args.get('state')
+    code_verifier = session.get('code_verifier')
+    access_token = get_access_token(code, code_verifier)
     session['access_token'] = access_token
+    
     return redirect(url_for('spotify.generate_playlist'))
 
 @auth_bp.route('/logout')
@@ -35,13 +55,14 @@ def logout():
         
     return 'Logged out successfully!'
 
-def get_access_token(code):
+def get_access_token(code, code_verifier):
     token_url = 'https://accounts.spotify.com/api/token' # URL to get token from Spotify accounts
     token_data = {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': REDIRECT_URI,
         'client_id': CLIENT_ID,
+        'code_verifier': code_verifier,
         'client_secret': CLIENT_SECRET,
     }
     token_response = requests.post(token_url, data=token_data)
